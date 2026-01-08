487e7d497084ecb96254abc921a57355ee24a1a7
Baruh Ifraimov <baruh.ifraimov@gmail.com>
2026-01-08 11:39:42 +0200

    [v0.14.4] Bug fix: Fix import path for NotificationCard and update tab name from "Favorite" to "Favorites" in TabConfigs

--- client/src/navigation/TabConfigs.js
+++ client/src/navigation/TabConfigs.js
@@ -8,7 +8,7 @@ export const tabConfigs = {
 	Home: { icon: "home", activeColor: "#1A73E8" }, // Orange (brand)
 	Community: { icon: "people", activeColor: "#1E8E3E" }, // Blue (social)
 	Routes: { icon: "map", activeColor: "#673AB7" }, // Green (travel)
-	Favorite: { icon: "bookmark", activeColor: "#F9A825" }, // i think bookmark icon looks more modern, dor thinks like a old head and wants a star, if star is chosen as the favorite among our friends, chagne it to 'star'
+	Favorites: { icon: "bookmark", activeColor: "#F9A825" }, // i think bookmark icon looks more modern, dor thinks like a old head and wants a star, if star is chosen as the favorite among our friends, chagne it to 'star'
 	Profile: { icon: "person", activeColor: "#E67C00" }, // Purple (personal)
 };
 
